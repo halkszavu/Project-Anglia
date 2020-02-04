a9683429ef651d3a8aaa73ef40f9c20ed20e192b
Botond Lajos Perényi <halkszavu@outlook.com>
2020-02-04 22:22:51 +0100

    Add project files.

---
+++ Project Anglia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Anglia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
